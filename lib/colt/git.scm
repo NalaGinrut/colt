d5cf0aca30c677385262ea4ddf8d49096047b55e
Nala Ginrut <nalaginrut@gmail.com>
2017-06-20 03:13:00 +0800

    implement post fetcher

--- lib/colt/git.scm
+++ lib/colt/git.scm
@@ -15,9 +15,11 @@
 ;;  along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (colt git)
+  #:use-module (artanis env)
   #:use-module (colt cmd)
   #:use-module ((rnrs) #:select (define-record-type))
   #:use-module (ice-9 match)
+  #:use-module (ice-9 rdelim)
   #:use-module (srfi srfi-1)
   #:export ())
 
@@ -27,6 +29,30 @@
 
 (define-record-type git-object
   (fields mode type oid file))
+
+(define-record-type comment
+  (fields timestamp author email site content))
+
+(define-syntax-rule (-> l)
+  (string-trim-both (cdr (string-split l #\:))))
+
+(define-syntax-rule (rdline port)
+  (-> (read-line port)))
+
+(define (new-comment oid)
+  (call-with-input-string
+   (raw-cmd git show ,oid)
+   (lambda (port)
+     (let ((timestamp (rdline port))
+           (author (rdline port))
+           (email (rdline port))
+           (site (rdline port))
+           (content (-> (read-string-all port))))
+       (make-comment timestamp
+                     author
+                     email
+                     site
+                     content)))))
 
 (define (get-object-by-oid oid obj-list)
   (any (lambda (o) (and (string=? oid (git-object-oid o)) o)) obj-list))
@@ -44,9 +70,14 @@
                     "BUG: Invalid git object format ~a" o))))
    (cmd git ls-tree ,branch)))
 
-(define (get-all-sub-oids oid)
-  
-  )
+(define (get-all-comment-oids coid)
+  (fold (lambda (x p)
+          (match x
+            (("tree" _) p)
+            ((oid) (cons x p))
+            (else (throw 'artanis-err 500 git/get-comments
+                         "Invalid comments tree ~a" x))))
+        (cmd-result-contents (cmd git show coid))))
 
 (define (git/get-content oid)
   (let ((cid (format #f "~a:content" oid)))
@@ -54,11 +85,38 @@
 
 (define (git/get-comments oid)
   (let* ((coid (format #f "~a:comments" oid))
-         (comments (get-all-sub-oids coid)))
-    
+         (comment-oids (get-all-comment-oids coid)))
+    (map new-comment comment-oids)))
 
-    )
-  )
+(define (git/get-meta-data oid)
+  (define-syntax-rule (->list l)
+    (map string-trim-both (string-split l #\,)))
+  (define (parse-meta-data moid)
+    (call-with-input-string
+     (raw-cmd git show ,moid)
+     (lambda (port)
+       (let ((timestamp (rdline port))
+             (tags (->list (rdline port)))
+             (title (rdline port))
+             (name (rdline port))
+             (comment-status (rdline port)))
+         (make-meta-data timestamp
+                         tags
+                         title
+                         name
+                         comment-status)))))
+  (let ((moid (format #f "~a:metadata" oid)))
+    (parse-meta-data moid)))
+
+(define (get-post oid)
+  (let ((content (git/get-content oid))
+        (comments (git/get-comments oid))
+        (meta-data (git/get-meta-data oid)))
+    (make-post meta-data content comments)))
+
+(define (git/get-posts)
+  (let ((ol (git-ls-tree)))
+    (map get-post ol)))
 
 (define (git-db-init)
   (cmd git init --bare))
@@ -68,5 +126,4 @@
   (chdir (current-blog-repo)))
 
 (define (git/commit-change)
-  (cmd )
-  )
+  #t)
